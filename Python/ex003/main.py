2b37a2bbf44bec386d5726148f04483e0b866c8c
Iliya <garon7777@Ya.ru>
2022-11-05 16:01:23 +0300

    Добавлена 003 задача и способ её применения

---
+++ Python/ex003/main.py
@@ -0,0 +1,10 @@
+def count_str_letters_in_another_str(str1, str2):
+    letters = {}
+    for str2_letter in str2:
+        for str1_letter in str1:
+            if str2_letter == str1_letter:
+                if not(str1_letter in letters):
+                    letters[str1_letter] = 1
+                else:
+                    letters[str1_letter] += 1
+    return letters
